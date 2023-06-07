e09d5bdd40c685d8967208af3c50a144a98b6e83
macias34 <propiesek@gmail.com>
2023-06-07 17:08:20 +0200

    Created Product superclass

---
+++ src/Product.java
@@ -0,0 +1,17 @@
+public class Product {
+    private String type;
+    private double price;
+
+    public Product(String type, double price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getPrice(int quantity) {
+        return price * quantity;
+    }
+}
